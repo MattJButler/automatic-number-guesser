04bfc15166e6ca53638463d850c71f297648393d
matt <mattbutler3576@gmail.com>
2021-10-01 13:48:00 +1000

    modified:   numberGuesser.py

--- numberGuesser.py
+++ numberGuesser.py
@@ -8,7 +8,19 @@ def main(userInput):
     userInput = int(userInput)
     if userInput < 1000 and userInput > 1:
         guess = random.randint(0,1000)    
-        for i in range(1000):
+        while guess != userInput:
+                if userInput > guess:   # if the guess was too small:
+                    guess = random.randint(guess, 1000)
+                    print(f"guess: ",guess, "is lower")
+                elif userInput < guess: # if the guess is too big
+                    guess = random.randint(1, guess)
+                    print(f"guess: ",guess, "is higher")
+        if userInput == guess:
+                    print(f"The answer is",guess)        
+        
+        
+        #for i in range(1000):
+        '''
             while guess != userInput:
                 if userInput > guess:   # if the guess was too small:
                     guess = random.randint(guess, 1000)
@@ -16,6 +28,8 @@ def main(userInput):
                 elif userInput < guess: # if the guess is too big
                     guess = random.randint(1, guess)
                     print(f"guess: ",guess, "is higher")
+            '''
+        
 
 userInput = input("pick a number between 1 - 1000:  ")
 if userInput == "":
